78cf5185862e863a77d53401fd150e8f1948dce5
ecsimsw <ecsimsw@gmail.com>
2022-01-09 00:01:05 +0900

    feat : GroupResponse 변경

--- src/main/java/com/example/frestraw/group/GroupResponse.java
+++ src/main/java/com/example/frestraw/group/GroupResponse.java
@@ -5,21 +5,37 @@ import java.util.stream.Collectors;
 
 public class GroupResponse {
 
+    private final Long id;
     private final String name;
+    private final String ownerEmail;
 
-    public GroupResponse(String name) {
+    public GroupResponse(Long id, String name, String ownerEmail) {
+        this.id = id;
         this.name = name;
+        this.ownerEmail = ownerEmail;
     }
 
     public static GroupResponse of(Group group) {
-        return new GroupResponse("hihi");
+        return new GroupResponse(
+                group.getId(),
+                group.getName(),
+                group.getOwnerEmail()
+        );
     }
 
     public static List<GroupResponse> listOf(List<Group> groups) {
-        return groups.stream().map(it->of(it)).collect(Collectors.toList());
+        return groups.stream().map(GroupResponse::of).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
     }
 }
