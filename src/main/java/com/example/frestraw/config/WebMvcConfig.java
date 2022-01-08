6398efb80d653219df4de8bbb178cac8b2b92392
pkeugine <pkeugine@gmail.com>
2022-01-09 05:28:26 +0900

    feat : cors 처리

--- src/main/java/com/example/frestraw/config/WebMvcConfig.java
+++ src/main/java/com/example/frestraw/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(allowedOrigins)
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
