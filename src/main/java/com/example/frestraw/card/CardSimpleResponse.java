6681dca41f24eca14da5caf769bd47f18de3e837
pkeugine <pkeugine@gmail.com>
2022-01-09 10:17:15 +0900

    feat : CardSimpleResponse에 사진 추가

--- src/main/java/com/example/frestraw/card/CardSimpleResponse.java
+++ src/main/java/com/example/frestraw/card/CardSimpleResponse.java
@@ -8,19 +8,21 @@ public class CardSimpleResponse {
     private Long id;
     private String name;
     private String gender;
+    private String imageUrl;
 
-    public CardSimpleResponse(Long id, String name, String gender) {
+    public CardSimpleResponse(Long id, String name, String gender, String imageUrl) {
         this.id = id;
         this.name = name;
         this.gender = gender;
+        this.imageUrl = imageUrl;
     }
 
     public static CardSimpleResponse of(Card card) {
-        return new CardSimpleResponse(card.getId(), card.getName(), card.getGender());
+        return new CardSimpleResponse(card.getId(), card.getName(), card.getGender(), card.getImage());
     }
 
     public static List<CardSimpleResponse> listOf(List<Card> cardsInGroup) {
-        return cardsInGroup.stream().map(it -> of(it)).collect(Collectors.toList());
+        return cardsInGroup.stream().map(CardSimpleResponse::of).collect(Collectors.toList());
     }
 
     public Long getId() {
@@ -33,5 +35,9 @@ public class CardSimpleResponse {
 
     public String getName() {
         return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
